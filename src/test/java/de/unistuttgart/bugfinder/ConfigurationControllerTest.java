425da92669c99db7535054316f41545b1e0f336b
Max Kästner <st172096@stud.uni-stuttgart.de>
2022-09-16 13:01:55 +0200

    create a first unit test class

---
+++ src/test/java/de/unistuttgart/bugfinder/ConfigurationControllerTest.java
@@ -0,0 +1,113 @@
+package de.unistuttgart.bugfinder;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import de.unistuttgart.bugfinder.code.Code;
+import de.unistuttgart.bugfinder.code.CodeDTO;
+import de.unistuttgart.bugfinder.code.word.Word;
+import de.unistuttgart.bugfinder.code.word.WordDTO;
+import de.unistuttgart.bugfinder.configuration.Configuration;
+import de.unistuttgart.bugfinder.configuration.ConfigurationDTO;
+import de.unistuttgart.bugfinder.configuration.ConfigurationMapper;
+import de.unistuttgart.bugfinder.configuration.ConfigurationRepository;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+import java.util.UUID;
+import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+@AutoConfigureMockMvc
+@SpringBootTest
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class ConfigurationControllerTest {
+
+  @Autowired
+  private MockMvc mvc;
+
+  @Autowired
+  private ConfigurationMapper configurationMapper;
+
+  @Autowired
+  private ConfigurationRepository configurationRepository;
+
+  private final String API_URL = "/configurations";
+  private ObjectMapper objectMapper;
+  private Configuration initialConfig;
+  private ConfigurationDTO initialConfigDTO;
+
+  @BeforeEach
+  public void createBasicData() {
+    final Code code1 = new Code();
+    code1.setWords(
+      Arrays.asList(
+        new Word("public"),
+        new Word(" "),
+        new Word("void"),
+        new Word(" "),
+        new Word("sayHello()"),
+        new Word(" "),
+        new Word("{"),
+        new Word("\n"),
+        new Word("}")
+      )
+    );
+    final Configuration configuration = new Configuration();
+    configuration.setCodes(Set.of(code1));
+    initialConfig = configurationRepository.save(configuration);
+    initialConfigDTO = configurationMapper.toDTO(initialConfig);
+
+    objectMapper = new ObjectMapper();
+  }
+
+  @BeforeAll
+  @AfterAll
+  public void deleteBasicData() {
+    configurationRepository.deleteAll();
+  }
+
+  @Test
+  void getConfigurations() throws Exception {
+    final MvcResult result = mvc
+      .perform(get(API_URL).contentType(MediaType.APPLICATION_JSON))
+      .andExpect(status().isOk())
+      .andReturn();
+
+    final List<ConfigurationDTO> configurations = Arrays.asList(
+      objectMapper.readValue(result.getResponse().getContentAsString(), ConfigurationDTO[].class)
+    );
+
+    assertSame(1, configurations.size());
+    assertEquals(initialConfigDTO, configurations.get(0));
+  }
+
+  @Test
+  void getConfiguration() throws Exception {
+    final MvcResult result = mvc
+      .perform(get(API_URL + "/" + initialConfig.getId()).contentType(MediaType.APPLICATION_JSON))
+      .andExpect(status().isOk())
+      .andReturn();
+
+    final ConfigurationDTO configuration = objectMapper.readValue(
+      result.getResponse().getContentAsString(),
+      ConfigurationDTO.class
+    );
+
+    assertEquals(initialConfigDTO, configuration);
+  }
+
+  @Test
+  void getConfiguration_DoesNotExist_ThrowsNotFound() throws Exception {
+    mvc
+      .perform(get(API_URL + "/" + UUID.randomUUID()).contentType(MediaType.APPLICATION_JSON))
+      .andExpect(status().isNotFound());
+  }
+}
