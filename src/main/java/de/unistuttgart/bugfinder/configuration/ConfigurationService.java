1cfab6fcada5b8835b32c73d0bccd1418107a1cc
IlijazM <mehmedovic.ilijaz@gmail.com>
2022-09-29 12:26:18 +0200

    Revert "add unit test to endpoints using the view model"
    This reverts commit 2ff57e049d493590e3d929bd26676e39d520a91d.

--- src/main/java/de/unistuttgart/bugfinder/configuration/ConfigurationService.java
+++ src/main/java/de/unistuttgart/bugfinder/configuration/ConfigurationService.java
@@ -13,205 +13,204 @@ import de.unistuttgart.bugfinder.solution.Solution;
 import de.unistuttgart.bugfinder.solution.SolutionRepository;
 import de.unistuttgart.bugfinder.solution.bug.Bug;
 import de.unistuttgart.bugfinder.solution.bug.BugRepository;
+import java.util.*;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
-import java.util.*;
-
 @Service
 @Slf4j
 public class ConfigurationService {
 
-    @Autowired
-    private ConfigurationRepository configurationRepository;
+  @Autowired
+  private ConfigurationRepository configurationRepository;
 
-    @Autowired
-    private ConfigurationMapper configurationMapper;
+  @Autowired
+  private ConfigurationMapper configurationMapper;
 
-    @Autowired
-    private CodeMapper codeMapper;
+  @Autowired
+  private CodeMapper codeMapper;
 
-    @Autowired
-    private CodeRepository codeRepository;
+  @Autowired
+  private CodeRepository codeRepository;
 
-    @Autowired
-    private SolutionRepository solutionRepository;
+  @Autowired
+  private SolutionRepository solutionRepository;
 
-    @Autowired
-    private WordRepository wordRepository;
+  @Autowired
+  private WordRepository wordRepository;
 
-    @Autowired
-    private BugRepository bugRepository;
+  @Autowired
+  private BugRepository bugRepository;
 
-    /**
-     * Get a configuration by its id.
-     *
-     * @param id the id of the configuration
-     * @return the found configuration
-     * @throws ResponseStatusException (404) when configuration with its id does not exist
-     */
-    public Configuration getConfiguration(final UUID id) {
-        return configurationRepository
-                .findById(id)
-                .orElseThrow(() ->
-                        new ResponseStatusException(HttpStatus.NOT_FOUND, String.format("Configuration with id %s not found.", id))
-                );
-    }
+  /**
+   * Get a configuration by its id.
+   *
+   * @param id the id of the configuration
+   * @return the found configuration
+   * @throws ResponseStatusException (404) when configuration with its id does not exist
+   */
+  public Configuration getConfiguration(final UUID id) {
+    return configurationRepository
+      .findById(id)
+      .orElseThrow(() ->
+        new ResponseStatusException(HttpStatus.NOT_FOUND, String.format("Configuration with id %s not found.", id))
+      );
+  }
 
-    /**
-     * @return all configurations as list of DTOs
-     */
-    public List<ConfigurationDTO> findAll() {
-        log.debug("get all configurations");
-        return configurationMapper.toDTO(configurationRepository.findAll());
-    }
+  /**
+   * @return all configurations as list of DTOs
+   */
+  public List<ConfigurationDTO> findAll() {
+    log.debug("get all configurations");
+    return configurationMapper.toDTO(configurationRepository.findAll());
+  }
 
-    /**
-     * Get the configuration by its id as DTO.
-     *
-     * @param id the id of the configuration
-     * @return the found configuration as DTO
-     * @throws ResponseStatusException (404) when configuration with its id does not exist
-     */
-    public ConfigurationDTO find(final UUID id) {
-        log.debug("get configuration {}", id);
-        Configuration configuration = getConfiguration(id);
-        return configurationMapper.toDTO(configuration);
-    }
+  /**
+   * Get the configuration by its id as DTO.
+   *
+   * @param id the id of the configuration
+   * @return the found configuration as DTO
+   * @throws ResponseStatusException (404) when configuration with its id does not exist
+   */
+  public ConfigurationDTO find(final UUID id) {
+    log.debug("get configuration {}", id);
+    Configuration configuration = getConfiguration(id);
+    return configurationMapper.toDTO(configuration);
+  }
 
-    /**
-     * Save a configuration.
-     *
-     * @param configurationDTO the configuration to save
-     * @return the saved configuration as DTO
-     */
-    public ConfigurationDTO save(final ConfigurationDTO configurationDTO) {
-        log.debug("create configuration {}", configurationDTO);
-        return configurationMapper.toDTO(configurationRepository.save(configurationMapper.fromDTO(configurationDTO)));
-    }
+  /**
+   * Save a configuration.
+   *
+   * @param configurationDTO the configuration to save
+   * @return the saved configuration as DTO
+   */
+  public ConfigurationDTO save(final ConfigurationDTO configurationDTO) {
+    log.debug("create configuration {}", configurationDTO);
+    return configurationMapper.toDTO(configurationRepository.save(configurationMapper.fromDTO(configurationDTO)));
+  }
 
-    public ConfigurationDTO build(final ConfigurationVM configurationVM) {
-        Set<Code> codesToPersist = new HashSet<>(configurationVM.getCodes().size());
-        for (CodeVM codeVM : configurationVM.getCodes()) {
-            List<Word> wordsToPersistToCode = new ArrayList<>(codeVM.getWords().size());
-            Set<Bug> bugsToPersistToSolution = new HashSet<>();
-            bugsToPersistToSolution = new HashSet<>();
-            for (List<WordVM> row : codeVM.getWords()) {
-                // check if the row only contains blank strings
-                // the first row will never contain only blank strings since it comes from the lecture interface
-                if (row.stream().allMatch(wordVM -> wordVM.getCorrectValue().isBlank())) {
-                    continue;
-                }
-                // if it is not the first row in the code we add a new line
-                if (codeVM.getWords().indexOf(row) != 0) {
-                    wordsToPersistToCode.add(wordRepository.save(new Word(null, "\n")));
-                }
-                for (WordVM wordVM : row) {
-                    // check if the word is blank
-                    // the first word will never be blank since it comes from the lecture interface
-                    if (wordVM.getCorrectValue().isBlank()) {
-                        continue;
-                    }
-                    // if it is not the first word in the row we add a space
-                    if (row.indexOf(wordVM) != 0) {
-                        wordsToPersistToCode.add(wordRepository.save(new Word(null, " ")));
-                    }
-                    String displayValue = wordVM.getDisplayValue() != null ? wordVM.getDisplayValue() : wordVM.getCorrectValue();
-                    Word word = new Word(null, displayValue);
-                    word = wordRepository.save(word);
-                    wordsToPersistToCode.add(word);
-                    if (wordVM.getErrorType() != null) {
-                        Bug bug = new Bug(word, wordVM.getErrorType(), wordVM.getCorrectValue());
-                        bug = bugRepository.save(bug);
-                        bugsToPersistToSolution.add(bug);
-                    }
-                }
-            }
-            Code code = new Code(null, wordsToPersistToCode);
-            codesToPersist.add(code);
-            Solution solution = new Solution(null, bugsToPersistToSolution, code);
-            code = codeRepository.save(code);
-            solution = solutionRepository.save(solution);
+  public ConfigurationDTO build(final ConfigurationVM configurationVM) {
+    Set<Code> codesToPersist = new HashSet<>(configurationVM.getCodes().size());
+    for (CodeVM codeVM : configurationVM.getCodes()) {
+      List<Word> wordsToPersistToCode = new ArrayList<>(codeVM.getWords().size());
+      Set<Bug> bugsToPersistToSolution = new HashSet<>();
+      bugsToPersistToSolution = new HashSet<>();
+      for (List<WordVM> row : codeVM.getWords()) {
+        // check if the row only contains blank strings
+        // the first row will never contain only blank strings since it comes from the lecture interface
+        if (row.stream().allMatch(wordVM -> wordVM.getCorrectValue().isBlank())) {
+          continue;
         }
-        Configuration configuration = new Configuration(null, codesToPersist);
-        return configurationMapper.toDTO(configurationRepository.save(configuration));
-    }
-
-    /**
-     * Deletes the configuration with the given ID, if present.
-     *
-     * @param id the ID of the configuration to delete
-     * @return the deleted configuration, if found
-     * @throws ResponseStatusException (204 - NO_CONTENT) if the configuration was not found
-     */
-    public ConfigurationDTO delete(final UUID id) {
-        log.info("delete configuration {}", id);
-        Configuration configuration = configurationRepository
-                .findById(id)
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NO_CONTENT));
-        configurationRepository.deleteById(id);
-        return configurationMapper.toDTO(configuration);
-    }
-
-    /**
-     * Get all codes of a configuration.
-     *
-     * @param id the configuration id
-     * @return the codes of the specified configuration
-     */
-    public List<CodeDTO> getCodes(final UUID id) {
-        log.debug("get codes from configuration {}", id);
-        final Configuration configuration = getConfiguration(id);
-        return codeMapper.toDTO(configuration.getCodes());
-    }
-
-    public ConfigurationVM getViewModel(UUID id) {
-        log.debug("get configuration view model {}", id);
-        final Configuration configuration = getConfiguration(id);
-        final ConfigurationVM configurationVM = new ConfigurationVM(new ArrayList<>());
-        for (Code code : configuration.getCodes()) {
-            final CodeVM codeVM = new CodeVM(new ArrayList<>());
-            final Solution solution = solutionRepository
-                    .findByCodeId(code.getId())
-                    .orElseThrow(() ->
-                            new ResponseStatusException(
-                                    HttpStatus.NOT_FOUND,
-                                    String.format("Solution with code id %s not found.", code.getId())
-                            )
-                    );
-
-            List<WordVM> row = new ArrayList<>();
-            for (Word word : code.getWords()) {
-                if (word.getWord().equals("\n")) {
-                    codeVM.getWords().add(row);
-                    row = new ArrayList<>();
-                    continue;
-                }
-                Optional<Bug> bug = solution
-                        .getBugs()
-                        .stream()
-                        .filter(b -> b.getWord().getId().equals(word.getId()))
-                        .findFirst();
-                if (word.getWord().equals(" ") && bug.isEmpty()) {
-                    continue;
-                }
-                final WordVM wordVM = new WordVM();
-                if (bug.isPresent()) {
-                    wordVM.setErrorType(bug.get().getErrorType());
-                    wordVM.setCorrectValue(bug.get().getCorrectValue());
-                    wordVM.setDisplayValue(word.getWord());
-                } else {
-                    wordVM.setCorrectValue(word.getWord());
-                }
-                row.add(wordVM);
-            }
-            codeVM.getWords().add(row);
-            configurationVM.getCodes().add(codeVM);
+        // if it is not the first row in the code we add a new line
+        if (codeVM.getWords().indexOf(row) != 0) {
+          wordsToPersistToCode.add(wordRepository.save(new Word(null, "\n")));
         }
-
-        return configurationVM;
+        for (WordVM wordVM : row) {
+          // check if the word is blank
+          // the first word will never be blank since it comes from the lecture interface
+          if (wordVM.getCorrectValue().isBlank()) {
+            continue;
+          }
+          // if it is not the first word in the row we add a space
+          if (row.indexOf(wordVM) != 0) {
+            wordsToPersistToCode.add(wordRepository.save(new Word(null, " ")));
+          }
+          String displayValue = wordVM.getDisplayValue() != null ? wordVM.getDisplayValue() : wordVM.getCorrectValue();
+          Word word = new Word(null, displayValue);
+          word = wordRepository.save(word);
+          wordsToPersistToCode.add(word);
+          if (wordVM.getErrorType() != null) {
+            Bug bug = new Bug(word, wordVM.getErrorType(), wordVM.getCorrectValue());
+            bugsToPersistToSolution.add(bug);
+            bug = bugRepository.save(bug);
+          }
+        }
+      }
+      Code code = new Code(null, wordsToPersistToCode);
+      codesToPersist.add(code);
+      Solution solution = new Solution(null, bugsToPersistToSolution, code);
+      code = codeRepository.save(code);
+      solution = solutionRepository.save(solution);
     }
+    Configuration configuration = new Configuration(null, codesToPersist);
+    return configurationMapper.toDTO(configurationRepository.save(configuration));
+  }
+
+  /**
+   * Deletes the configuration with the given ID, if present.
+   *
+   * @param id the ID of the configuration to delete
+   * @return the deleted configuration, if found
+   * @throws ResponseStatusException (204 - NO_CONTENT) if the configuration was not found
+   */
+  public ConfigurationDTO delete(final UUID id) {
+    log.info("delete configuration {}", id);
+    Configuration configuration = configurationRepository
+      .findById(id)
+      .orElseThrow(() -> new ResponseStatusException(HttpStatus.NO_CONTENT));
+    configurationRepository.deleteById(id);
+    return configurationMapper.toDTO(configuration);
+  }
+
+  /**
+   * Get all codes of a configuration.
+   *
+   * @param id the configuration id
+   * @return the codes of the specified configuration
+   */
+  public List<CodeDTO> getCodes(final UUID id) {
+    log.debug("get codes from configuration {}", id);
+    final Configuration configuration = getConfiguration(id);
+    return codeMapper.toDTO(configuration.getCodes());
+  }
+
+  public ConfigurationVM getViewModel(UUID id) {
+    log.debug("get configuration view model {}", id);
+    final Configuration configuration = getConfiguration(id);
+    final ConfigurationVM configurationVM = new ConfigurationVM(new ArrayList<>());
+    for (Code code : configuration.getCodes()) {
+      final CodeVM codeVM = new CodeVM(new ArrayList<>());
+      final Solution solution = solutionRepository
+        .findByCodeId(code.getId())
+        .orElseThrow(() ->
+          new ResponseStatusException(
+            HttpStatus.NOT_FOUND,
+            String.format("Solution with code id %s not found.", code.getId())
+          )
+        );
+
+      List<WordVM> row = new ArrayList<>();
+      for (Word word : code.getWords()) {
+        if (word.getWord().equals(" ")) {
+          continue;
+        }
+        if (word.getWord().equals("\n")) {
+          codeVM.getWords().add(row);
+          row = new ArrayList<>();
+          continue;
+        }
+        Optional<Bug> bug = solution
+          .getBugs()
+          .stream()
+          .filter(b -> b.getWord().getId().equals(word.getId()))
+          .findFirst();
+        final WordVM wordVM = new WordVM();
+        if (bug.isPresent()) {
+          wordVM.setErrorType(bug.get().getErrorType());
+          wordVM.setCorrectValue(bug.get().getCorrectValue());
+          wordVM.setDisplayValue(word.getWord());
+        } else {
+          wordVM.setCorrectValue(word.getWord());
+        }
+        row.add(wordVM);
+      }
+      codeVM.getWords().add(row);
+      configurationVM.getCodes().add(codeVM);
+    }
+
+    return configurationVM;
+  }
 }
--- src/test/java/de/unistuttgart/bugfinder/ConfigurationControllerTest.java
+++ src/test/java/de/unistuttgart/bugfinder/ConfigurationControllerTest.java
@@ -1,5 +1,9 @@
 package de.unistuttgart.bugfinder;
 
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 import de.unistuttgart.bugfinder.code.Code;
 import de.unistuttgart.bugfinder.code.CodeDTO;
@@ -9,11 +13,10 @@ import de.unistuttgart.bugfinder.configuration.Configuration;
 import de.unistuttgart.bugfinder.configuration.ConfigurationDTO;
 import de.unistuttgart.bugfinder.configuration.ConfigurationMapper;
 import de.unistuttgart.bugfinder.configuration.ConfigurationRepository;
-import de.unistuttgart.bugfinder.configuration.vm.CodeVM;
-import de.unistuttgart.bugfinder.configuration.vm.ConfigurationVM;
-import de.unistuttgart.bugfinder.configuration.vm.WordVM;
-import de.unistuttgart.bugfinder.solution.SolutionDTO;
-import de.unistuttgart.bugfinder.solution.bug.ErrorType;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+import java.util.UUID;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -21,15 +24,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
-
-import java.util.Arrays;
-import java.util.List;
-import java.util.Set;
-import java.util.UUID;
-
-import static org.junit.jupiter.api.Assertions.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @AutoConfigureMockMvc
 @SpringBootTest
@@ -55,17 +49,17 @@ public class ConfigurationControllerTest {
   public void createBasicData() {
     final Code code1 = new Code();
     code1.setWords(
-            Arrays.asList(
-                    new Word("public"),
-                    new Word(" "),
-                    new Word("void"),
-                    new Word(" "),
-                    new Word("sayHello()"),
-                    new Word(" "),
-                    new Word("{"),
-                    new Word("\n"),
-                    new Word("}")
-            )
+      Arrays.asList(
+        new Word("public"),
+        new Word(" "),
+        new Word("void"),
+        new Word(" "),
+        new Word("sayHello()"),
+        new Word(" "),
+        new Word("{"),
+        new Word("\n"),
+        new Word("}")
+      )
     );
     final Configuration configuration = new Configuration();
     configuration.setCodes(Set.of(code1));
@@ -78,18 +72,18 @@ public class ConfigurationControllerTest {
   @BeforeAll
   @AfterEach
   public void deleteBasicData() {
-//        configurationRepository.deleteAll();
+    configurationRepository.deleteAll();
   }
 
   @Test
   void getConfigurations() throws Exception {
     final MvcResult result = mvc
-            .perform(get(API_URL).contentType(MediaType.APPLICATION_JSON))
-            .andExpect(status().isOk())
-            .andReturn();
+      .perform(get(API_URL).contentType(MediaType.APPLICATION_JSON))
+      .andExpect(status().isOk())
+      .andReturn();
 
     final List<ConfigurationDTO> configurations = Arrays.asList(
-            objectMapper.readValue(result.getResponse().getContentAsString(), ConfigurationDTO[].class)
+      objectMapper.readValue(result.getResponse().getContentAsString(), ConfigurationDTO[].class)
     );
 
     assertSame(1, configurations.size());
@@ -99,13 +93,13 @@ public class ConfigurationControllerTest {
   @Test
   void getConfiguration() throws Exception {
     final MvcResult result = mvc
-            .perform(get(API_URL + "/" + initialConfig.getId()).contentType(MediaType.APPLICATION_JSON))
-            .andExpect(status().isOk())
-            .andReturn();
+      .perform(get(API_URL + "/" + initialConfig.getId()).contentType(MediaType.APPLICATION_JSON))
+      .andExpect(status().isOk())
+      .andReturn();
 
     final ConfigurationDTO configuration = objectMapper.readValue(
-            result.getResponse().getContentAsString(),
-            ConfigurationDTO.class
+      result.getResponse().getContentAsString(),
+      ConfigurationDTO.class
     );
 
     assertEquals(initialConfigDTO, configuration);
@@ -114,33 +108,33 @@ public class ConfigurationControllerTest {
   @Test
   void getConfiguration_DoesNotExist_ThrowsNotFound() throws Exception {
     mvc
-            .perform(get(API_URL + "/" + UUID.randomUUID()).contentType(MediaType.APPLICATION_JSON))
-            .andExpect(status().isNotFound());
+      .perform(get(API_URL + "/" + UUID.randomUUID()).contentType(MediaType.APPLICATION_JSON))
+      .andExpect(status().isNotFound());
   }
 
   @Test
   void createConfiguration() throws Exception {
     CodeDTO newCode = new CodeDTO();
     newCode.setWords(
-            Arrays.asList(
-                    new WordDTO("public"),
-                    new WordDTO(" "),
-                    new WordDTO("void"),
-                    new WordDTO(" "),
-                    new WordDTO("sayGoodbye()"),
-                    new WordDTO(" "),
-                    new WordDTO("{"),
-                    new WordDTO("\n"),
-                    new WordDTO("}")
-            )
+      Arrays.asList(
+        new WordDTO("public"),
+        new WordDTO(" "),
+        new WordDTO("void"),
+        new WordDTO(" "),
+        new WordDTO("sayGoodbye()"),
+        new WordDTO(" "),
+        new WordDTO("{"),
+        new WordDTO("\n"),
+        new WordDTO("}")
+      )
     );
     final ConfigurationDTO newConfiguration = new ConfigurationDTO();
     newConfiguration.setCodes(List.of(newCode));
     mvc
-            .perform(
-                    post(API_URL).content(objectMapper.writeValueAsString(newConfiguration)).contentType(MediaType.APPLICATION_JSON)
-            )
-            .andExpect(status().isCreated());
+      .perform(
+        post(API_URL).content(objectMapper.writeValueAsString(newConfiguration)).contentType(MediaType.APPLICATION_JSON)
+      )
+      .andExpect(status().isCreated());
 
     assertSame(2, configurationRepository.findAll().size());
   }
@@ -148,74 +142,16 @@ public class ConfigurationControllerTest {
   @Test
   void deleteConfiguration() throws Exception {
     final MvcResult result = mvc
-            .perform(delete(API_URL + "/" + initialConfig.getId()).contentType(MediaType.APPLICATION_JSON))
-            .andExpect(status().isOk())
-            .andReturn();
+      .perform(delete(API_URL + "/" + initialConfig.getId()).contentType(MediaType.APPLICATION_JSON))
+      .andExpect(status().isOk())
+      .andReturn();
 
     final ConfigurationDTO configuration = objectMapper.readValue(
-            result.getResponse().getContentAsString(),
-            ConfigurationDTO.class
+      result.getResponse().getContentAsString(),
+      ConfigurationDTO.class
     );
 
     assertEquals(initialConfigDTO, configuration);
     assertTrue(configurationRepository.findById(initialConfig.getId()).isEmpty());
-  }
-
-  @Test
-  void saveAndGetVM() throws Exception {
-    final ConfigurationVM viewModel = new ConfigurationVM(
-            List.of(
-                    new CodeVM(
-                            List.of(
-                                    List.of(
-                                            new WordVM("public", "pubvik", ErrorType.LEXICAL),
-                                            new WordVM("static", null, null),
-                                            new WordVM("void", null, null),
-                                            new WordVM("main", null, null),
-                                            new WordVM("{", null, null)
-                                    ),
-                                    List.of(
-                                            new WordVM("<tab>", null, null),
-                                            new WordVM("System.out.println", null, null),
-                                            new WordVM("(\"Hello, world\")", null, null),
-                                            new WordVM(";", " ", ErrorType.SYNTAX)
-                                    ),
-                                    List.of(new WordVM("}", null, null))
-                            )
-                    )
-            )
-    );
-
-    final MvcResult result = mvc
-            .perform(
-                    post(API_URL + "/build")
-                            .content(objectMapper.writeValueAsString(viewModel))
-                            .contentType(MediaType.APPLICATION_JSON)
-            )
-            .andExpect(status().isCreated())
-            .andReturn();
-
-    ConfigurationDTO configuration = objectMapper.readValue(result.getResponse().getContentAsString(), ConfigurationDTO.class);
-    List<String> expectedResult = List.of(
-            "pubvik", " ", "static", " ", "void", " ", "main", " ", "{", "\n", "<tab>", " ", "System.out.println", " ", "(\"Hello, world\")", " ", " ", "\n", "}"
-    );
-    for (int i = 0; i < expectedResult.size(); i++) {
-      assertEquals(expectedResult.get(i), configuration.getCodes().get(0).getWords().get(i).getWord(), "Word at index " + i + " is not equal. Expected: " + expectedResult.get(i) + ", actual: " + configuration.getCodes().get(0).getWords().get(i).getWord());
-    }
-    MvcResult solutionResponse = mvc
-            .perform(get(API_URL + "/" + configuration.getId() + "/codes/" + configuration.getCodes().get(0).getId() + "/solutions").contentType(MediaType.APPLICATION_JSON))
-            .andExpect(status().isOk())
-            .andReturn();
-
-    SolutionDTO solution = objectMapper.readValue(solutionResponse.getResponse().getContentAsString(), SolutionDTO.class);
-    assertEquals(2, solution.getBugs().size());
-
-    MvcResult vmFromApiResult = mvc
-            .perform(get(API_URL + "/vm/" + configuration.getId()).contentType(MediaType.APPLICATION_JSON))
-            .andExpect(status().isOk())
-            .andReturn();
-
-    ConfigurationVM vmFromApi = objectMapper.readValue(vmFromApiResult.getResponse().getContentAsString(), ConfigurationVM.class);
-    assertEquals(viewModel, vmFromApi);
   }
 }
